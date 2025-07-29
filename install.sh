96a8b683801db78cf57ea362ea342a0dfb2b6c26
Aditya Narayan <aditya.narayan@juspay.in>
2025-07-29 19:30:39 +0530

    Make install.sh executable

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-chmod +x diffChecker.py
-cp diffChecker.py /usr/local/bin/diff-tool
+chmod +x diffCheck.py
+cp diffCheck.py /usr/local/bin/diff-tool
 
 
-echo "✅ diffChecker installed to /usr/local/bin"
-echo "➡️  Usage: diffChecker old.txt new.txt"
+echo "✅ diffCheck installed to /usr/local/bin"
+echo "➡️  Usage: diffCheck old.txt new.txt"
